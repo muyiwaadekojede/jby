cca9ce0433177544c5d057448a6d525309386184
ProfinSEO <muyiwaadekojede@gmail.com>
2025-07-23 18:50:38 +0100

    forms for two type of user

--- components/onboarding/OnboardingForm.tsx
+++ components/onboarding/OnboardingForm.tsx
@@ -1,20 +1,53 @@
+"use client";
 import Image from "next/image";
 import Logo from "@/public/logo.png";
 import { Card, CardContent } from "../ui/card";
+import { useState } from "react";
+import { UserTypeSelection } from "./UserTypeForm";
 
-
+type UserSelectionType = "recruiter" | "jobSeeker" | null;
 
 export function OnboardingForm () {
+    const [step, setStep] = useState(1);
+    const [useType, setUserType] = useState<UserSelectionType>(null);
+
+    function handleUserTypeSelection(type: UserSelectionType) {
+        setUserType(type);
+        setStep(2);
+    }
+
+    function renderStep() {
+        switch(step) {
+            case 1:
+            return <UserTypeSelection />;
+
+            case 2: 
+            
+            return userType == "company" ?(
+            <p>User as a recruiter</p> 
+         ) : ( 
+            <p>User is a job seeker</p>
+         );
+
+
+            default: return null; 
+
+        }
+
+    }
+
     return (
         <>
         <div className="flex items-center gap-4 mb-10">
             <Image src={Logo} alt="Jobland Logo" width={50} height={50} />
             <h1>
-                Job<span className="text-primary">Marshal</span>
+                Job<span className="text-primary">Land</span>
             </h1>
         </div>
-        <Card>
-            <CardContent className="max-w-lg w-full"></CardContent>
+        <Card  className="max-w-lg w-full">
+            <CardContent className="p-6">
+                {renderStep()}
+            </CardContent>
         </Card>
         </>
     );
---
+++ components/onboarding/UserTypeForm.tsx
@@ -0,0 +1,13 @@
+export function UserTypeSelection() {
+    return (
+        <div>
+            <div className="text-center space-y-2">
+                <h2 className="text-2xl font-bold">Welcome! Lets Get Started</h2>
+                <p className="text-muted-foreground">Choose how to use our platform either as a jobseeker or recruiter looking to recruit talents</p>
+            </div>
+            
+            
+
+        </div>
+    )
+}
