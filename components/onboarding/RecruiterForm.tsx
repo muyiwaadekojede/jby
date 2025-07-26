53e5dfc3d16c784ebe603f4a1e78d5ce6aca0741
ProfinSEO <muyiwaadekojede@gmail.com>
2025-07-26 15:37:53 +0100

    Initial development of the recruiter form

--- components/onboarding/RecruiterForm.tsx
+++ components/onboarding/RecruiterForm.tsx
@@ -1,4 +1,15 @@
+import { useForm } from "react-hook-form";
+import {} from "@hookform/resolvers/zod"
+
 export function RecruiterForm() {
+
+
+    const form = useForm({
+        resolver: zodResolver ()
+
+    })
+
+
     return (
         <div>
             <h1>
