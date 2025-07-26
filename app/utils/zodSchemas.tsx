e3210a260ca2434cacf85545cbec65e38b786c09
ProfinSEO <muyiwaadekojede@gmail.com>
2025-07-26 18:21:58 +0100

    still on recruiters sign up form

--- app/utils/zodSchemas.tsx
+++ app/utils/zodSchemas.tsx
@@ -1,11 +1,11 @@
 import z from "zod";
 
-export const recruiterSchema = z.object ();({
+export const recruiterSchema = z.object ({
     name: z.string().min(2, "Company name must be at least 2 characters"),
     location: z.string().min(1, "Location must be real and given"),
     about: z.string().min(10, "Share a description of the company you represent or work at"),
 
     logo: z.string().min(2, "Upload the logo of the company you represent"),
     website: z.string().url("Enter a real or operational url"),
-    nxAccount: z.string().optional(),
+    xAccount: z.string().optional(),
 });
--- components/onboarding/RecruiterForm.tsx
+++ components/onboarding/RecruiterForm.tsx
@@ -1,20 +1,155 @@
 import { useForm } from "react-hook-form";
-import {} from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { recruiterSchema } from "@/app/utils/zodSchemas";
+import {z} from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "@/components/ui/Input";
+import { Select } from "@radix-ui/react-select";
+import { SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+import { countryList } from "@/app/utils/contries";
+import { Textarea } from "../ui/textarea";
 
 export function RecruiterForm() {
 
 
-    const form = useForm({
-        resolver: zodResolver ()
-
-    })
+    const form = useForm<z.infer<typeof recruiterSchema>>({
+        resolver: zodResolver(recruiterSchema),
+        defaultValues: {
+            about: "",
+            location: "",
+            logo: "",
+            name: "",
+            website: "",
+            xAccount: "",
+        },
+    });
 
 
     return (
-        <div>
-            <h1>
-                hello
-            </h1>
-        </div>
+        <Form {...form}>
+            <form className="space-y-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field } ) => (
+                        <FormItem>
+                            <FormLabel>
+                               Recruiting Company Name
+                            </FormLabel>
+                            <FormControl>
+                                <input placeholder="Enter Recruiting Company Name" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+
+                                        <FormField
+                    control={form.control}
+                    name="location"
+                    render={({ field } ) => (
+                        <FormItem>
+                            <FormLabel>
+                               Recruiting Company Location
+                            </FormLabel>
+                            <Select 
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                            >
+                            <FormControl>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Select Location" />
+                                </SelectTrigger>
+
+                            </FormControl>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>
+                                        Worldwide
+                                    </SelectLabel>
+                                    <SelectItem value={"worldwide"}>
+                                        <span>
+                                            🌍
+                                        </span>
+                                        <span>
+                                            Worldwide/Remote
+                                        </span>
+                                    </SelectItem>
+                                </SelectGroup>
+                                <SelectGroup>
+                                    <SelectLabel>
+                                        Location
+                                    </SelectLabel>
+                                    {countryList.map((country) =>(
+                                        <SelectItem key={country.code} value={country.name}>
+                                            <span>
+                                                {country.flagEmoji}
+                                            </span>
+                                            <span className="pl-2">
+                                                {country.name}
+                                            </span>
+                                        </SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                            </Select>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                </div>
+
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                    <FormField
+                    control={form.control}
+                    name="Website"
+                    render={({ field } ) => (
+                        <FormItem>
+                            <FormLabel>
+                               Recruiting Company Website
+                            </FormLabel>
+                            <FormControl>
+                                <input placeholder="https://yourrecruitingcompanywebsite" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <FormField
+                    control={form.control}
+                    name="xAccount"
+                    render={({ field } ) => (
+                        <FormItem>
+                            <FormLabel>
+                               X account for your Recruiting Company
+                            </FormLabel>
+                            <FormControl>
+                                <input placeholder="@recruitingcompany" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />        
+            </div>
+                    <FormField
+                    control={form.control}
+                    name="about"
+                    render={({ field } ) => (
+                        <FormItem>
+                            <FormLabel>
+                               About
+                            </FormLabel>
+                            <FormControl>
+                                <Textarea placeholder="Give a description about the company you are recruiting for" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+            </form>
+        </Form>
     );
 }
